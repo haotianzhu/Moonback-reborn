57ecf0d1305c1ed68cc206283ec68b2d1ab806b9
haotianzhu <haotianzhu@users.noreply.github.com>
2019-04-27 19:12:44 -0600

    feat: add post model, two urls 1. get api/posts  2. post api/posts (#12)

--- server/server.js
+++ server/server.js
@@ -1,18 +1,26 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const api = require('./routes/api')
+const mongoose = require('mongoose')
 const cors = require('cors')
 const PORT = 3000
 const app = express()
+const postApi = require('./routes/postController')
+const database = 'mongodb://yz:qaz98765432@ds155213.mlab.com:55213/db1'
+
+// init connetction to remote database
+mongoose.connect(database, { useNewUrlParser: true }, error => {
+    if (error) {
+        console.error(error)
+    } else {
+        console.log("connected")
+    }
+})
 
 app.use(cors())
 app.use(bodyParser.json())
-app.use('/api',api)
+// add api controllers
+app.use('/api/posts', postApi)
 
-app.get('/', function(req, res) {
-  res.send('hello')
-})
-
-app.listen(PORT,function(){
-    console.log('server running on localhost'+PORT)
+app.listen(PORT, function () {
+    console.log('server running on localhost' + PORT)
 })
