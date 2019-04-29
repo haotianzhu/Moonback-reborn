31f10c219bc1ebdd67fed5cfd86d0d4959c0c26c
haotianzhu <zhuhaotian19970215@gmail.com>
2019-04-28 21:21:53 -0600

    fix: error not throw

--- server/routes/postController.js
+++ server/routes/postController.js
@@ -3,55 +3,51 @@ const postRouter = express.Router()
 const Post = require('../models/post')
 
 function pasrseUrlToQuery(params) {
-  let query = { select: {}, limit: 100, skip: 0, sort: {} }
-  for (var key in params) {
-    value = params[key]
-    switch (key) {
-      case "limit":
-        query.limit = parseInt(value);
-        break;
-      case "skip":
-        query.skip = parseInt(value);
-        break;
-      case "sort":
-        query.sort = value;
-        break;
-      default:
-        query.select[key] = value;
+    let query = { select: {}, limit: 100, skip: 0, sort: {} }
+    for (var key in params) {
+        value = params[key]
+        switch (key) {
+        case "limit":
+            query.limit = parseInt(value);
+            break;
+        case "skip":
+            query.skip = parseInt(value);
+            break;
+        case "sort":
+            query.sort = value;
+            break;
+        default:
+            query.select[key] = value;
+        }
     }
-  }
-  return query
+    return query
 };
 
 // get api/posts return all posts
 postRouter.get('/', (req, res) => {
-  let query = pasrseUrlToQuery(req.query)
+    let query = pasrseUrlToQuery(req.query)
 
-  Post.execute(query, (error, data) => {
-    if (error) {
-      throw Error(error);
-    }
-    if (data) {
-      res.status(200).send({ posts: data, length: data.length, query: "findAllPosts" })
-    }
-  })
+    Post.execute(query, (error, data) => {
+        if (error) {
+            throw Error(error);
+        } else {
+            res.status(200).send({ posts: data, length: data.length, query: "findAllPosts" })
+        }
+    })
 })
 
 // post api/posts create a new post
 postRouter.post('/', (req, res) => {
-  let data = req.body;
-
-  if (data.query === "createNewPost") {
+    let data = req.body;
     let newPost = new Post(data.post);
     newPost.save((error, data) => {
-      if (error) {
-        console.log(error)
-      } else {
-        res.status(200).send({ post: data, query: "createNewPost" })
-      }
-    })
-  }
-})
+        if (error) {
+            console.log(error)
+        } else {
+            res.status(200).send({ post: data, query: "createNewPost" })
+        }
+    }) 
+});
 
 
 
