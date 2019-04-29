def5ec1f12882eb41db9ce89ae202263fc3850df
haotianzhu <zhuhaotian19970215@gmail.com>
2019-04-28 23:17:25 -0600

    fix: del query

--- server/routes/userController.js
+++ server/routes/userController.js
@@ -5,42 +5,35 @@ const User = require('../models/user')
 // api/authentication/signup
 userRouter.post('/signup', (req, res) => {
     let data = req.body;
-    if (data.query === "signUp") {
-        let newUser = new User(data.user);
-        console.log(newUser.username)
-        console.log(newUser.password)
-        newUser.save((error, data) => {
-            if (error) {
-                console.log(error)
-                res.status(400).send({ user:data, query: "signUp",status:"unsucessful" })
-            } else {
-                res.status(200).send({ user:data, query: "signUp",status:"sucessful" })
-            }
-        })
-    }else{
-      res.status(400).send({query: "signUp",status:"unsupported query" })
-    }
+    let newUser = new User(data.user);
+    console.log(newUser.username,data.user)
+    console.log(newUser.password)
+    newUser.save((error, data) => {
+        if (error) {
+            console.log(error)
+            res.status(400).send({ user:data, query: "signUp",status:"unsucessful" })
+        } else {
+            res.status(200).send({ user:data, query: "signUp",status:"sucessful" })
+        }
+    })
+    
 })
 
 // api/authentication/signin
 userRouter.post('/signin', async (req, res) => {
     let data = req.body;
-    if (data.query === "signIn") {
-        let signinUser = new User(data.user);
-        await User.findOne({ username: signinUser.username}, (error, user) => {
-        if (error) throw error;
-          // verify password
-        User.validatePassword(user.password, signinUser.password, function(error, isMatch) {
-        if (error) {
-          res.status(403).send({ user:signinUser.username, query: "signIn", status:"unsucessful"})
-        }else {
-          res.status(200).send({ user:signinUser.username, query: "signIn", status:"sucessful"})
-        }
-        })
-      });
-    }else{
-      res.status(400).send({query: "signIn",status:"unsupported query" })
-    }
+    let signinUser = new User(data.user);
+    await User.findOne({ username: signinUser.username}, (error, user) => {
+      if (error) throw error;
+        // verify password
+      User.validatePassword(user.password, signinUser.password, function(error, isMatch) {
+      if (error) {
+        res.status(403).send({ user:signinUser.username, query: "signIn", status:"unsucessful"})
+      }else {
+        res.status(200).send({ user:signinUser.username, query: "signIn", status:"sucessful"})
+      }
+      })
+    });
 })
 
   module.exports = userRouter
