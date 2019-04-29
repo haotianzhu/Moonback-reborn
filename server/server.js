94222df5429f9260d8fd70c069f8948e6bb214e9
haotianzhu <haotianzhu@users.noreply.github.com>
2019-04-28 21:48:20 -0600

    Fix/post api throw error bug (#20)
    * fix: error not throw
    
    * feat: add timeout controller
    
    * fix: add timeout

--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ const PORT = 3000
 const app = express()
 const postApi = require('./routes/postController')
 const database = 'mongodb://yz:qaz98765432@ds155213.mlab.com:55213/db1'
+const timeout = require('connect-timeout'); //express v4
 
 // init connetction to remote database
 mongoose.connect(database, { useNewUrlParser: true }, error => {
@@ -17,10 +18,24 @@ mongoose.connect(database, { useNewUrlParser: true }, error => {
 })
 
 app.use(cors())
+app.use(timeout('5s')) 
 app.use(bodyParser.json())
+app.use(haltOnTimedout);
 // add api controllers
 app.use('/api/posts', postApi)
 
+
+
+app.use(haltOnTimedout)
 app.listen(PORT, function () {
     console.log('server running on localhost' + PORT)
 })
+
+function haltOnTimedout (error, req, res, next) {
+    //https://www.npmjs.com/package/connect-timeout
+    if (!req.timedout) {
+        next()
+    } else {
+        res.status(408).send({message: "timeout!"})
+    }
+}   
