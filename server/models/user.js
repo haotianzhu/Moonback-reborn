a69dffe7b0cf214afcd54dda3749019179f4ed9a
haotianzhu <haotianzhu@users.noreply.github.com>
2019-04-28 20:55:12 -0600

    Fix: user model password validation  (#18)
    * fix: user model validate password
    
    * fix: user model's password validation bug

--- server/models/user.js
+++ server/models/user.js
@@ -1,6 +1,9 @@
 
 var bcrypt = require('bcrypt');
-var SALT_WORK_FACTOR = 10;
+const jwt = require('jsonwebtoken');
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var SALT_WORK_FACTOR = 11;
 
 
 var userSchema = new Schema({
@@ -10,37 +13,33 @@ var userSchema = new Schema({
     modifyDate: { type: Date, default: Date.now },
 });
 
-userSchema.statics.execute = function(query, callback) {
+userSchema.statics.execute = function (query, callback) {
     return this.find(query.select).skip(query.skip).limit(query.limit).sort(query.sort).exec(callback)
 };
 
-//https://www.mongodb.com/blog/post/password-authentication-with-mongoose-part-1
-userSchema.pre('save', function(next) {
-    var user = this;
+userSchema.method.validatePassword = function (candidatePassword, callback) {
+    bcrypt.compare(candidatePassword, this.password, (error, isMatch) => {
+        if (error) return callback(error);
+        callback(null, isMatch);
+    });
+}
 
+//https://www.mongodb.com/blog/post/password-authentication-with-mongoose-part-1
+userSchema.pre('save', function (next) {
+    var user = this;
     // only hash the password if it has been modified (or is new)
     if (!user.isModified('password')) return next();
-
-    // generate a salt
+    // hash the password along with our new salt
     bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
         if (err) return next(err);
-
-        // hash the password using our new salt
+        // hash the password along with our new salt
         bcrypt.hash(user.password, salt, function(err, hash) {
             if (err) return next(err);
-
             // override the cleartext password with the hashed one
             user.password = hash;
             next();
         });
     });
 });
-
-userSchema.methods.comparePassword = function(candidatePassword, callback) {
-    bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
-        if (err) return callback(err);
-        callback(null, isMatch);
-    });
-};
 
 module.exports = mongoose.model('user', userSchema, 'users');
