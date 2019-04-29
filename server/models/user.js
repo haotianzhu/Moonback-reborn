b2ba704dc9741e072cf5721af8b140267269ae21
haotianzhu <zhuhaotian19970215@gmail.com>
2019-04-28 20:48:29 -0600

    fix: user model's password validation bug

--- server/models/user.js
+++ server/models/user.js
@@ -13,34 +13,33 @@ var userSchema = new Schema({
     modifyDate: { type: Date, default: Date.now },
 });
 
-userSchema.statics.execute = (query, callback) => {
+userSchema.statics.execute = function (query, callback) {
     return this.find(query.select).skip(query.skip).limit(query.limit).sort(query.sort).exec(callback)
 };
 
-UsersSchema.methods.setPassword = (password) => {
-    var salt = bcrypt.genSaltSync(SALT_WORK_FACTOR);
-    this.password = bcrypt.hashSync(password, salt);
-};
-
-serSchema.methods.validatePassword = (candidatePassword, callback) => {
-    bcrypt.compareSync(candidatePassword, this.password, (error, isMatch) => {
+userSchema.method.validatePassword = function (candidatePassword, callback) {
+    bcrypt.compare(candidatePassword, this.password, (error, isMatch) => {
         if (error) return callback(error);
         callback(null, isMatch);
     });
 }
 
 //https://www.mongodb.com/blog/post/password-authentication-with-mongoose-part-1
-userSchema.pre('save', (next) => {
+userSchema.pre('save', function (next) {
     var user = this;
-    await user.setPassword(user.password);
-    next();
-});
-
-userSchema.methods.comparePassword = (candidatePassword, callback) => {
-    bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
-        if (err) return callback(err);
-        callback(null, isMatch);
+    // only hash the password if it has been modified (or is new)
+    if (!user.isModified('password')) return next();
+    // hash the password along with our new salt
+    bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
+        if (err) return next(err);
+        // hash the password along with our new salt
+        bcrypt.hash(user.password, salt, function(err, hash) {
+            if (err) return next(err);
+            // override the cleartext password with the hashed one
+            user.password = hash;
+            next();
+        });
     });
-};
+});
 
 module.exports = mongoose.model('user', userSchema, 'users');
