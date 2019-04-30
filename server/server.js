841b910445de1ed354d321f2059487eb84a22348
haotianzhu <zhuhaotian19970215@gmail.com>
2019-04-29 23:08:50 -0600

    feat: add token

--- server/server.js
+++ server/server.js
@@ -8,6 +8,8 @@ const postApi = require('./routes/postController')
 const userApi = require('./routes/userController')
 const database = 'mongodb://yz:qaz98765432@ds155213.mlab.com:55213/db1'
 const timeout = require('connect-timeout'); //express v4
+const jwt = require('jsonwebtoken')
+var cookieParser = require('cookie-parser')
 
 // init connetction to remote database
 mongoose.connect(database, { useNewUrlParser: true }, error => {
@@ -19,25 +21,46 @@ mongoose.connect(database, { useNewUrlParser: true }, error => {
 })
 
 app.use(cors())
-app.use(timeout('5s')) 
+app.use(timeout('5s'))
+app.use(cookieParser())
 app.use(bodyParser.json())
 app.use(haltOnTimedout);
 // add api controllers
-app.use('/api/posts', postApi)
+app.use('/api/posts', verifyToken, postApi)
 app.use('/api/authentication', userApi)
-
-
-
 app.use(haltOnTimedout)
+
 app.listen(PORT, function () {
     console.log('server running on localhost' + PORT)
 })
 
-function haltOnTimedout (error, req, res, next) {
+function verifyToken(req, res, next) {
+    // verify the Json Token
+    if (!req.headers.authorization) {
+        return res.status(401).send('Unauthorized request');
+    }
+    let token = req.headers.authorization.split(' ')[1];
+    console.log(token)
+    if (token === 'null') {
+        return res.status(401).send('Unauthorized request');
+    }
+    try {
+        let payload = jwt.verify(token, 'secret');
+        console.log(payload)
+        req.userId = payload.userid;
+    } catch (error) {
+        console.log('verifyToken => ' + error)
+        return res.status(401).send({ error: error });
+    }
+    next();
+}
+
+function haltOnTimedout(error, req, res, next) {
     //https://www.npmjs.com/package/connect-timeout
+
     if (!req.timedout) {
         next()
     } else {
-        res.status(508).send({message: "timeout!"})
+        res.status(508).send({ message: "timeout!" })
     }
 }   
