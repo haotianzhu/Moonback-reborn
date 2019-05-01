fe7de16f4a9e9df4a98959392268a77a413c232b
Yuntian Zhang <zytwh2012@gmail.com>
2019-04-30 22:25:14 -0600

    fix npm dependency

--- Moonback/src/app/app.module.ts
+++ Moonback/src/app/app.module.ts
@@ -4,7 +4,6 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { AuthenticationComponent } from './authentication/authentication.component';
 import { PostComponent } from './post/post.component';
 import { HomeComponent } from './home/home.component';
 import { SigninComponent } from './signin/signin.component';
@@ -13,7 +12,6 @@ import { SignupComponent } from './signup/signup.component';
 @NgModule({
   declarations: [
     AppComponent,
-    AuthenticationComponent,
     PostComponent,
     HomeComponent,
     SigninComponent,
