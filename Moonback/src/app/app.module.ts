b1fc039bfc3284a20f8f2858c93010d5648d2bf6
Yuntian Zhang <zytwh2012@gmail.com>
2019-04-30 21:16:59 -0600

    sign in sign up html and css

--- Moonback/src/app/app.module.ts
+++ Moonback/src/app/app.module.ts
@@ -6,12 +6,18 @@ import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AuthenticationComponent } from './authentication/authentication.component';
 import { PostComponent } from './post/post.component';
+import { HomeComponent } from './home/home.component';
+import { SigninComponent } from './signin/signin.component';
+import { SignupComponent } from './signup/signup.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AuthenticationComponent,
-    PostComponent
+    PostComponent,
+    HomeComponent,
+    SigninComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
--- Moonback/src/app/authentication/authentication.component.ts
+++ Moonback/src/app/authentication/authentication.component.ts
@@ -1,15 +1,82 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, AfterViewInit, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Location } from '@angular/common';
+
+// import { RegistrareService } from './registrate.service';
+// import { LoginService } from './login.service';
+// import { TokenService } from './token.service';
 
 @Component({
-  selector: 'app-authentication',
+  selector: 'app-login',
   templateUrl: './authentication.component.html',
   styleUrls: ['./authentication.component.css']
 })
-export class AuthenticationComponent implements OnInit {
 
-  constructor() { }
+export class AuthenticationComponent implements OnInit {
+  loginInRequestData = {email: '', password: ''};
+  signUpRequestData = {email: '', password: ''};
+  private loginIsHidden = false ;
+  private rememberMeOn = true;
+  constructor(private router: Router, private location: Location) {}
 
   ngOnInit() {
+    // const loggedIn = this.tokenserver.loggedIn();
+    // const loggedIn = false;
+    // if (loggedIn) {
+    //   this.router.navigate(['/']);
+    // } else {
+    //   if (this.location.isCurrentPathEqualTo('/login')) {
+    //     this.loginIsHidden = false ;
+    //   } else if (this.location.isCurrentPathEqualTo('/signup')) {
+    //     this.loginIsHidden = true ;
+    //   } else {
+    //     console.log('error login unknown url', this.router.url );
+    //     this.loginIsHidden = false ;
+    //   }
+    // }
+    // Cute is justice. Three cheers for the dancing girl.
+    // const dancingGrilsGif = document.getElementById('girls');
+    // dancingGrilsGif.style.position = 'fixed';
   }
+
+  // logIn() {
+  //   this.log.loginInRequest(this.loginInRequestData)
+  //     .subscribe(
+  //       // todo: return a json user, check user's active status
+  //       // is actived, save it to cookie and redirected to router
+  //       res => {
+  //         if (res) {
+  //           // if not 'remember me'
+  //           if (this.rememberMeOn) {
+  //             localStorage.setItem('refreshToken', res.data.refreshToken);
+  //             localStorage.setItem('userId', this.loginInRequestData.email);
+  //             sessionStorage.removeItem('refreshToken'); // in case
+  //             sessionStorage.removeItem('userId');
+  //           } else {
+  //             sessionStorage.setItem('refreshToken', res.data.refreshToken);
+  //             sessionStorage.setItem('userId', this.loginInRequestData.email);
+  //             localStorage.removeItem('refreshToken'); // in case
+  //             localStorage.removeItem('userId'); // in case
+  //           }
+  //           sessionStorage.setItem('accessToken', res.data.accessToken);
+  //           this.router.navigate(['/']);
+  //         }
+  //       },
+  //       error => console.log(error, 'logIn() error')
+  //     );
+  // }
+
+  // signUp() {
+  //   this.reg.signUpRequest(this.signUpRequestData)
+  //     .subscribe(
+  //       res => {
+  //         this.loginIsHidden = false;
+  //       },
+  //       error => {
+  //         console.log(error, 'signUp() error');
+  //         this.loginIsHidden = true;
+  //       }
+  //     );
+  // }
 
 }
