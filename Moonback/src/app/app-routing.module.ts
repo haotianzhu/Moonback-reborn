ff3f91a6f5564c103538eda739b455af0ad4369c
haotianzhu <zhuhaotian19970215@gmail.com>
2019-05-03 01:52:37 -0600

    feat: add post-list

--- Moonback/src/app/app-routing.module.ts
+++ Moonback/src/app/app-routing.module.ts
@@ -3,15 +3,17 @@ import { Routes, RouterModule } from '@angular/router';
 import { SigninComponent } from './authentication/signin/signin.component';
 import { SignoutComponent } from './authentication/signout/signout.component';
 import { SignupComponent } from './authentication/signup/signup.component';
+import { PostListComponent } from './post/post-list/post-list.component';
 
 const routes: Routes = [
   { path: 'signin', component: SigninComponent},
   { path: 'signout', component: SignoutComponent},
   { path: 'signup', component: SignupComponent},
+  { path: 'user/posts', component: PostListComponent},
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
---
+++ Moonback/src/app/post/post-list/post-list.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-post-list',
+  templateUrl: './post-list.component.html',
+  styleUrls: ['./post-list.component.css']
+})
+export class PostListComponent implements OnInit {
+  public postArray = new Array<any>();
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+    // api/post/user/id
+    console.log(environment.baseUrl)
+    var secondHeaders = new HttpHeaders({
+      'Authorization': 'bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6Imhhb3RpYW56aHVAcXEuY29tIiwiaWQiOiI1Y2M5MDE4ZTQ2YmY2NDIzOWZhZTg3MTIiLCJleHAiOjE1NTkyNjkwMjYsImlhdCI6MTU1NjY3NzAyNn0.uwtbg3ZanFRNe0Rk7zNVD5BJJ5vyvt2gCdzXgHel6TE'
+    });
+
+    this.http.get<any>(
+      `${environment.baseUrl + '/posts/user/' + '5cc9018e46bf64239fae8712'}`,
+      { headers: secondHeaders }
+    ).subscribe( (res) => {
+      if ( res.length > 0 ) {
+        this.postArray = res.posts;
+      }
+    })
+  }
+
+}
