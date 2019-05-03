29fee99f96c356f68a0fde0ec5903abc1a942e6e
haotianzhu <zhuhaotian19970215@gmail.com>
2019-05-03 14:44:38 -0600

    fix: use controllers

--- Moonback/src/app/authentication/signin/signin.component.ts
+++ Moonback/src/app/authentication/signin/signin.component.ts
@@ -3,7 +3,7 @@ import { HttpClient } from "@angular/common/http";
 import { environment } from 'src/environments/environment';
 import { AuthService } from '../auth.service';
 import { Router } from '@angular/router';
-import { FormGroup } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signin',
@@ -11,28 +11,42 @@ import { FormGroup } from '@angular/forms';
   styleUrls: ['./signin.component.css']
 })
 export class SigninComponent implements OnInit {
-  username = '';
-  password = '';
-
+  signinForm: FormGroup;
+  isCorrect = true;
   constructor(private http: HttpClient, private auth: AuthService, private router: Router) { }
 
   ngOnInit() {
     if (this.auth.isAuth()) this.router.navigate(['/']);
+    this.signinForm = new FormGroup({
+      username: new FormControl('', [
+        Validators.required,
+      ]),
+      password: new FormControl('', [Validators.required]),
+    });
   }
 
   onSignIn() {
-    this.http.post<any>(
-      `${environment.baseUrl + 'authentication/signin'}`,
-      { user: { username: this.username, password: this.password } },
-      { observe: 'response' }
-    ).subscribe((res) => {
-      if (res.status == 200) {
-        this.auth.setAuth(res.body.user);
-        if (this.auth.isAuth()) {
-          this.router.navigate(['/']);
+    if (this.signinForm.valid) {
+      this.http.post<any>(
+        `${environment.baseUrl + 'authentication/signin'}`,
+        { user: this.signinForm.value },
+        { observe: 'response' }
+      ).subscribe(
+        res => {
+          if (res.status == 200) {
+            this.auth.setAuth(res.body.user);
+            if (this.auth.isAuth()) {
+              this.router.navigate(['/']);
+            }
+          }
+        },
+        error => {
+          this.isCorrect = false;
         }
-      }
-    })
+        
+      )
+
+    }
   }
 
 }
