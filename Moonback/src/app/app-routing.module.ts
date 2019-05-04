418078fe7881874984014f3b3ddb4d8bca3bc243
haotianzhu <zhuhaotian19970215@gmail.com>
2019-05-03 19:56:19 -0600

    add post-list-ts

--- Moonback/src/app/app-routing.module.ts
+++ Moonback/src/app/app-routing.module.ts
@@ -6,10 +6,11 @@ import { SignupComponent } from './authentication/signup/signup.component';
 import { PostListComponent } from './post/post-list/post-list.component';
 
 const routes: Routes = [
+  { path: '', component: PostListComponent},
   { path: 'signin', component: SigninComponent},
   { path: 'signout', component: SignoutComponent},
   { path: 'signup', component: SignupComponent},
-  { path: 'user/posts', component: PostListComponent},
+  { path: 'user/:id', component: PostListComponent},
 ];
 
 @NgModule({
--- Moonback/src/app/post/post-list/post-list.component.ts
+++ Moonback/src/app/post/post-list/post-list.component.ts
@@ -1,7 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Renderer2, ElementRef, } from '@angular/core';
 import { environment } from 'src/environments/environment';
-import { HttpClient, HttpHeaders } from "@angular/common/http";
-import { Observable } from 'rxjs';
+import { HttpClient } from "@angular/common/http";
+import { concatMap, switchMap, map, mapTo, combineLatest, mergeMap, debounceTime } from 'rxjs/operators';
+import { AuthService } from 'src/app/authentication/auth.service';
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
+import { delay, async } from 'q';
+import { of, Observable, fromEvent, concat } from 'rxjs';
 
 @Component({
   selector: 'app-post-list',
@@ -9,25 +13,74 @@ import { Observable } from 'rxjs';
   styleUrls: ['./post-list.component.css']
 })
 export class PostListComponent implements OnInit {
-  public postArray = new Array<any>();
+  postArray = [];
+  isLoading = true;
+  userid = null;
+  postsRoute$: Observable<any>;
+  scroll$: Observable<any>;
 
-  constructor(private http: HttpClient) { }
+  constructor(
+    private http: HttpClient,
+    private auth: AuthService,
+    private router: Router,
+    private route: ActivatedRoute,
+    private rd: Renderer2,
+    private element: ElementRef) {
+  }
 
   ngOnInit() {
     // api/post/user/id
-    console.log(environment.baseUrl)
-    var secondHeaders = new HttpHeaders({
-      'Authorization': 'bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6Imhhb3RpYW56aHVAcXEuY29tIiwiaWQiOiI1Y2M5MDE4ZTQ2YmY2NDIzOWZhZTg3MTIiLCJleHAiOjE1NTkyNjkwMjYsImlhdCI6MTU1NjY3NzAyNn0.uwtbg3ZanFRNe0Rk7zNVD5BJJ5vyvt2gCdzXgHel6TE'
-    });
+    if (!this.auth.isAuth()) {
+      this.router.navigate(['/signin']);
+    }
 
-    this.http.get<any>(
-      `${environment.baseUrl + 'posts/user/' + '5cc9018e46bf64239fae8712'}`,
-      { headers: secondHeaders }
-    ).subscribe( (res) => {
-      if ( res.length > 0 ) {
-        this.postArray = res.posts;
+    //https://angular.io/guide/router
+    this.postsRoute$ = this.route.paramMap.pipe(
+      switchMap((params: ParamMap) => of(params.get('id')))
+    )
+    this.postsRoute$.subscribe(
+      (id) => {
+        if (id) { // with id
+          this.userid = id;
+          this.loadingPost(this.userid);
+        } else {
+          // home page
+          this.userid = this.auth.getAuth().id;
+          this.loadingPost(this.userid);
+        }
       }
-    })
+    )
+
+    this.scroll$ = fromEvent(document, 'scroll')
+      .pipe(
+        debounceTime(1000),
+        map(() => {
+          return (window.scrollY + window.innerHeight == document.body.scrollHeight);
+        })
+      )
+    this.scroll$.subscribe(
+      (fetchMore) => {
+        if (fetchMore) { // with id
+          this.isLoading = true;
+          this.loadingPost(this.userid);
+        }
+      }
+    )
+  }
+
+  async loadingPost(id) {
+    this.isLoading = await this.http.get<any>(`${environment.baseUrl + 'posts/user/' + id}`, { observe: 'response' })
+      .toPromise()
+      .then(async (res) => {
+        if (res.body.length > 0) {
+          // this.postArray = this.postArray.concat(res.body.posts);
+          this.postArray = res.body.posts;
+          return false
+        }
+      })
+      .catch((error) => {
+        return true
+      });
   }
 
 }
