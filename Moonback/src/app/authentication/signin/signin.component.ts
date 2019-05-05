5f29052127c0e11ac7c151dc36b5eb276dbb2851
haotianzhu <haotianzhu@users.noreply.github.com>
2019-05-05 16:18:34 -0600

    fix: sign in validation (#74)
    * fix bug
    
    * fixed

--- Moonback/src/app/authentication/signin/signin.component.ts
+++ Moonback/src/app/authentication/signin/signin.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Renderer2, ElementRef } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
@@ -16,7 +16,13 @@ import { AuthService } from '../shared/auth.service';
 export class SigninComponent implements OnInit {
   signinForm: FormGroup;
   isCorrect = true;
-  constructor(private http: HttpClient, private auth: AuthService, private router: Router) { }
+
+  constructor(
+    private http: HttpClient,
+    private auth: AuthService,
+    private router: Router,
+    private rd: Renderer2,
+    private element: ElementRef) { }
 
   ngOnInit() {
     if (this.auth.isAuth()) { this.router.navigate(['/']); }
@@ -26,6 +32,14 @@ export class SigninComponent implements OnInit {
       ]),
       password: new FormControl('', [Validators.required]),
     });
+  }
+
+  setRed(el, isAdd: boolean) {
+    if (isAdd) {
+      this.rd.addClass(el, 'control-warm');
+    } else {
+      this.rd.removeClass(el, 'control-warm');
+    }
   }
 
   onSignIn() {
@@ -45,9 +59,22 @@ export class SigninComponent implements OnInit {
         },
         error => {
           this.isCorrect = false;
+          this.signinForm.controls.username.markAsPristine();
+          this.signinForm.controls.password.markAsPristine();
         }
 
       );
+    } else {
+      if (this.signinForm.controls.password.errors) {
+        if (this.signinForm.controls.password.errors.required) {
+          this.signinForm.controls.password.markAsTouched();
+        }
+      }
+      if (this.signinForm.controls.username.errors) {
+        if (this.signinForm.controls.username.errors.required) {
+          this.signinForm.controls.username.markAsTouched();
+        }
+      }
     }
   }
 
