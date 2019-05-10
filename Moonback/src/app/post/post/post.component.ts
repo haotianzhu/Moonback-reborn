aaca513ef505acb11c7c6c2844f477e178030ef0
haotianzhu <zhuhaotian19970215@gmail.com>
2019-05-10 01:55:33 -0600

    fix bar

--- Moonback/src/app/post/post/post.component.ts
+++ Moonback/src/app/post/post/post.component.ts
@@ -11,63 +11,10 @@ import { NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 import { interval, merge, concat, of } from 'rxjs';
-import { mergeMapTo, takeUntil, mergeMap, map, mapTo, take, tap, switchMap, takeWhile } from 'rxjs/operators';
-import { async } from 'q';
-import { post } from 'selenium-webdriver/http';
+import { mergeMapTo, takeUntil, mergeMap, map, mapTo, take, tap, switchMap, takeWhile, max, min } from 'rxjs/operators';
+import { async, delay } from 'q';
+import { post, del } from 'selenium-webdriver/http';
 import { Location } from '@angular/common';
-
-
-
-// @Component({
-//   selector: 'app-post-modal-content',
-//   templateUrl: './post.content.html'
-// })
-
-// export class PostModalContent {  // tslint:disable-line:component-class-suffix
-//   @Input() post;
-//   @Input() isEditable;
-//   isEditMode = false;
-//   isPending = false;
-//   sideBarvalue = '0';
-
-//   constructor(
-//     public activeModal: NgbActiveModal,
-//     private http: HttpClient,
-//     private rd: Renderer2,
-//     private element: ElementRef) {
-//   }
-
-//   async onSave() {
-//     this.isPending = true;
-
-//     const update$ = this.http.patch<any>(
-//       `${environment.baseUrl + 'posts/' + this.post.id}`, { post: this.post },
-//       { observe: 'response' }
-//     ).pipe(
-//       map(res => res.status === 200)
-//     );
-//     const timer$ = interval(1000).pipe(take(5), map(x => 10 + x * 18));
-//     const result$ = merge(timer$, update$);
-//     await result$.subscribe(async (success) => {
-//       if (success === true) {
-//         return this.activeModal.close({ done: false, post: this.post });
-//       } else if (success === false) {
-//         return this.activeModal.close({ done: true });
-//       } else {
-//         this.sideBarvalue = success.toString();
-//       }
-//     });
-//     this.isPending = false;
-//   }
-
-//   onEdit() {
-//     this.isEditMode = this.isEditable;
-//   }
-
-//   onClose() {
-//     this.activeModal.close({ done: true });
-//   }
-// }
 
 
 @Component({
@@ -131,23 +78,37 @@ export class PostComponent implements OnInit {
 
   async onSave() {
     this.isPending = true;
+    this.sideBarvalue = '0';
     const update$ = this.http.patch<any>(
       `${environment.baseUrl + 'posts/' + this.post.id}`, { post: this.tmpPost },
       { observe: 'response' }
     ).pipe(
       map(res => res.status === 200)
     );
-    const timer$ = interval(1000).pipe(take(5), map(x => 10 + x * 18));
+    const timer$ = interval(500).pipe(take(19));
     const result$ = merge(timer$, update$);
     result$.subscribe(async (success) => {
+
       if (success === true) {
-        this.post = await Object.assign(this.tmpPost, this.post);
+        let isWait = false;
+        if (this.sideBarvalue !== '95') {
+          this.sideBarvalue = '95';
+          isWait = true;
+        }
+        this.post = await Object.assign({}, this.tmpPost);
+        if (isWait) {
+          await delay(100);
+        }
         this.isPending = false;
-        await this.onCancel();
+        this.onCancel();
       } else if (success === false) {
         this.isPending = false;
       } else {
-        this.sideBarvalue = success.toString();
+        let val = parseInt(this.sideBarvalue, 10) + 5;
+        if (val > 95) {
+          val = 95;
+        }
+        this.sideBarvalue = val.toString();
       }
     });
   }
