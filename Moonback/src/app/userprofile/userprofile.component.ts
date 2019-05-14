e7169d34384f9cfb6bf4940e5cf619c0886457b8
Yuntian Zhang <zytwh2012@gmail.com>
2019-05-13 20:59:52 -0600

    change password frot end done

--- Moonback/src/app/userprofile/userprofile.component.ts
+++ Moonback/src/app/userprofile/userprofile.component.ts
@@ -11,23 +11,31 @@ import { AuthService } from '../authentication/shared/auth.service';
   styleUrls: ['./userprofile.component.css']
 })
 export class UserprofileComponent implements OnInit {
+  verifyPasswordForm: FormGroup;
   changePasswordForm: FormGroup;
   userInfo: any;
-  isAuth = false;
+  isVerified = true;
 
-   constructor(
+  constructor(
     private http: HttpClient,
     private auth: AuthService,
     private router: Router) { }
 
   ngOnInit() {
     if (!this.auth.isAuth()) { this.router.navigate(['/signin']); }
-    console.log(typeof(this.auth.getAuth()));
     this.userInfo = this.auth.getAuth();
-    this.changePasswordForm = new FormGroup({
-      username: new FormControl({value: this.userInfo.username, disabled: true}, [Validators.required]),
-      password: new FormControl('', [Validators.required]),
-    });
+
+    if (!this.isVerified) {
+      this.verifyPasswordForm = new FormGroup({
+        username: new FormControl({ value: this.userInfo.username, disabled: true }, [Validators.required]),
+        password: new FormControl('', [Validators.required]),
+      });
+    } else {
+      this.changePasswordForm = new FormGroup({
+        password: new FormControl('', [Validators.required]),
+        confirmpassword: new FormControl('', [Validators.required])
+      });
+    }
   }
 
 }
