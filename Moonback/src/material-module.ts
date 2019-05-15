250531b59289829ed2e3fbbbd1f881d7b3529d48
Yuntian Zhang <zytwh2012@gmail.com>
2019-05-15 00:02:50 -0600

    Update material-module.ts

--- Moonback/src/material-module.ts
+++ Moonback/src/material-module.ts
@@ -1,11 +1,11 @@
-import {A11yModule} from '@angular/cdk/a11y';
-import {DragDropModule} from '@angular/cdk/drag-drop';
-import {PortalModule} from '@angular/cdk/portal';
-import {ScrollingModule} from '@angular/cdk/scrolling';
-import {CdkStepperModule} from '@angular/cdk/stepper';
-import {CdkTableModule} from '@angular/cdk/table';
-import {CdkTreeModule} from '@angular/cdk/tree';
-import {NgModule} from '@angular/core';
+import { A11yModule } from '@angular/cdk/a11y';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import { PortalModule } from '@angular/cdk/portal';
+import { ScrollingModule } from '@angular/cdk/scrolling';
+import { CdkStepperModule } from '@angular/cdk/stepper';
+import { CdkTableModule } from '@angular/cdk/table';
+import { CdkTreeModule } from '@angular/cdk/tree';
+import { NgModule } from '@angular/core';
 import {
   MatAutocompleteModule,
   MatBadgeModule,
@@ -90,7 +90,7 @@ import {
     ScrollingModule,
   ]
 })
-export class MoonMaterialModule {}
+export class MoonMaterialModule { }
 
 
 /**  Copyright 2019 Google Inc. All Rights Reserved.
