68dfa38951e0bc968391d5b5c6362335f28bbd30
haotianzhu <zhuhaotian19970215@gmail.com>
2019-05-17 05:01:30 -0600

    1

--- server/models/user.js
+++ server/models/user.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
-const bcrypt = require('bcrypt')
+var bcrypt = require('bcryptjs');
 const SALT_WORK_FACTOR = 10
 const jwt = require('jsonwebtoken')
 
