589addc7520419dfa4e4f5ddb08484d4d570424e
haotianzhu <zhuhaotian19970215@gmail.com>
2019-05-26 18:39:54 -0600

    f

---
+++ Moonback/src/app/utils/dialog/info-dialog.component.ts
@@ -0,0 +1,18 @@
+import {Component, Inject} from '@angular/core';
+import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material';
+
+@Component({
+  selector: 'app-info-dialog',
+  templateUrl: 'info-dialog.component.html',
+})
+export class InformationDialogComponent {
+
+  constructor(
+    public dialogRef: MatDialogRef<InformationDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any) {}
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+
+}
