7cf6a7bd6732695aec4bbe24aca24b5a6c3ef0b9
haotianzhu <zhuhaotian19970215@gmail.com>
2019-06-02 21:28:30 -0600

    fix email-bug

--- server/routes/emailController.js
+++ server/routes/emailController.js
@@ -3,8 +3,8 @@ const nodemailer = require('nodemailer')
 const jwt = require('jsonwebtoken')
 const emailRouter = express.Router()
 const User = require('../models/user')
+const Token = require('../models/token')
 const logger = require('../shared/logger')
-const EMAILPATH = '/api/email/v?token='
 
 var transporter = nodemailer.createTransport({
   host: 'smtp.gmail.com',
@@ -17,54 +17,40 @@ var transporter = nodemailer.createTransport({
   }
 })
 
-const verifyEmailToken = (token) => {
-  try {
-    let payload = jwt.verify(token, 'moonback-reborn-secrete')
-    return payload
-  } catch (error) {
-    logger.info('token is not correct')
-    return null
-  }
+const verifyEmailToken = (date) => {
+  const today = new Date()
+  return today.getTime() < date.getTime()
 }
 
 emailRouter.post('/s', (req, res) => {
-  var token = null
-  const today = new Date()
-  const expirationDate = new Date(today)
-  expirationDate.setDate(today.getDate() + 1)
   var targetUser = {}
-  if (req.body.id) {
-    targetUser._id = req.body.id
-  } else if (req.body.username) {
+  if (req.body.email && req.body.username) {
+    targetUser.email = req.body.email
     targetUser.username = req.body.username
   } else {
-    logger.info('=> api/email/s, no username and no id')
+    logger.info('=> api/email/s, no username and no email')
     return res.sendStatus(400)
   }
+  const message = (req.body.message) ? req.body.message : 'please confirm your email address. '
 
-  const protocol = (req.body.isHttps) ? 'https://' : 'http://'
-
-  User.findOneAndUpdate(targetUser, '-password -token', (error, data) => {
+  User.findOne(targetUser, '-password -token', (error, data) => {
     if (error) {
       logger.info('=> api/email/s', error)
       return res.sendStatus(520)
     }
     if (data) {
-      token = jwt.sign({
-        username: data.username,
-        id: data.id,
-        exp: parseInt(expirationDate.getTime() / 1000, 10)
-      }, 'moonback-reborn-secrete')
+      var token = new Token({ userId: data.id })
       if (!data.email) {
         logger.info('Email sent:  no email address')
         return res.sendStatus(400)
       }
+      token.save()
       if (token) {
         const mailOptions = {
           from: 'moonbackreborn@gmail.com',
           to: data.email,
           subject: 'MoonBack-reborn Email Vertification',
-          text: `${'please confirm your email address. ' + protocol + req.headers.host + EMAILPATH + token}`
+          text: `${message + token.value}`
         }
         transporter.sendMail(mailOptions, (error, info) => {
           if (error) {
@@ -73,7 +59,7 @@ emailRouter.post('/s', (req, res) => {
           }
           if (info) {
             logger.info('Email sent: ' + info.response)
-            return res.status(200).send({ email: data.email })
+            return res.status(200).send({ 'id': data.id })
           }
         })
       }
@@ -84,27 +70,43 @@ emailRouter.post('/s', (req, res) => {
   })
 })
 
-emailRouter.get('/v', (req, res) => {
+emailRouter.post('/v', (req, res) => {
   var token = null
-  try {
-    token = req.query.token
-  } catch (e) {
-    logger.error('No token in query')
+  if (req.body.value && req.body.id) {
+    token = {
+      value: req.body.value,
+      type: 'email',
+      userId: req.body.id
+    }
+  } else {
+    logger.info('=> api/email/s, no value and no id')
     return res.sendStatus(400)
   }
+
   if (token) {
-    const payload = verifyEmailToken(token)
-    if (payload) {
-      // true
-      User.findByIdAndUpdate(payload.id, { isActivated: true }, { useFindAndModify: false }, (error) => {
-        if (error) {
-          logger.info('GET api/email/v => 520 ', error)
-          res.sendStatus(520)
+    Token.findOne(token, (err, data) => {
+      if (err) {
+        logger.error('=> api/email/v', err)
+      }
+      if (data) {
+        if (verifyEmailToken(data.date)) {
+          User.findByIdAndUpdate(data.userId, { isActivated: true }, { useFindAndModify: false }, (error) => {
+            if (error) {
+              logger.error('GET api/email/v => 520 ', error)
+              res.sendStatus(520)
+            }
+            logger.info('GET api/email/v  => 200')
+            res.sendStatus(200)
+          })
+        } else {
+          logger.info('=> api/email/v 401 token expired')
+          res.sendStatus(401)
         }
-        logger.info('GET api/email/v  => 200')
-        res.status(200).send({ token })
-      })
-    }
+      } else {
+        logger.info('=> api/email/v 404 no token is found')
+        res.sendStatus(404)
+      }
+    })
   }
 })
 
