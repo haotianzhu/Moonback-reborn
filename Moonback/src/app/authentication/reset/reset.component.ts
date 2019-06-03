84f1c0c1ab5e7a551d742b414f7669fb2f30c763
Yuntian Zhang <zytwh2012@gmail.com>
2019-06-02 20:21:20 -0600

    v2

--- Moonback/src/app/authentication/reset/reset.component.ts
+++ Moonback/src/app/authentication/reset/reset.component.ts
@@ -10,8 +10,8 @@ import { environment } from 'src/environments/environment';
   styleUrls: ['./reset.component.css']
 })
 export class ResetComponent implements OnInit {
-  verifyEamilForm: FormGroup;
-  isVerficationEmailFail = false;
+  verifyForm: FormGroup;
+  isVerficationFail = false;
   isVerified = false;
   isSent = false;
   isActive: boolean;
@@ -22,24 +22,32 @@ export class ResetComponent implements OnInit {
     private http: HttpClient) { }
 
   ngOnInit() {
-    this.userInfo = this.auth.getAuth();
-    this.isActive = (this.userInfo.isActive === 'true');
-    this.verifyEamilForm = new FormGroup({
+    if(this.auth.isAuth()){
+      this.userInfo = this.auth.getAuth();
+      this.isActive = (this.userInfo.isActive === 'true');
+      this.verifyForm = new FormGroup({
+      username: new FormControl(this.userInfo.username, [Validators.required]),
       email: new FormControl('', [Validators.required, Validators.email])
     });
-  }
-  onVerifyEmail() {
-    if (this.verifyEamilForm.value.email === this.auth.getAuth().email) {
-      this.isVerified = true;
-    } else {
-      this.isVerficationEmailFail = true;
+    }else {
+      this.verifyForm = new FormGroup({
+        username: new FormControl('', [Validators.required]),
+        email: new FormControl('', [Validators.required, Validators.email])
+      });
     }
   }
-
+  onVerifyEmail() {
+    if (this.verifyForm.value.email === this.auth.getAuth().email) {
+      this.isVerified = true;
+    } else {
+      this.isVerficationFail = true;
+    }
+  }
+s
   onResentEmail() {
     this.http.post<any>(
       `${environment.baseUrl + 'email/s'}`,
-      { username: this.userInfo.username },
+      { user: this.verifyForm },
       { observe: 'response' }
     ).subscribe(
       res => {
@@ -50,5 +58,13 @@ export class ResetComponent implements OnInit {
       }, error => {
         console.log(error);
       });
+  }
+  onResetPassword(){
+    if(this.auth.isAuth()){
+
+    }
+    else{
+
+    }
   }
 }
